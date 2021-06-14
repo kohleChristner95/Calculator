a14e69cc35a6d2e6534dfcaa7515de5137d63b4e
Kohle Christner <kohlec@fireflyint.com>
2021-06-14 19:58:37 -0400

    Added ng-click to numbers to add to input box. Still need to be able to add zero. Currently doesn't show.

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 let myApp = angular.module('myApp', []);
 
-myApp.controller('btnController', function($scope, $rootScope, $log, btnService) {
+myApp.controller('btnController', function($scope, $rootScope, $log) {
+    $scope.zero = '0' 
+    $scope.one = '1'
+    $scope.two = '2'
+    $scope.three = '3'
+    $scope.four = '4'
+    $scope.five = '5'
+    $scope.six = '6'
+    $scope.seven = '7'
+    $scope.eight = '8'
+    $scope.nine = '9'
+    $scope.inputValue = '';
+    $scope.getInputValue = function () {
+        console.log($scope.inputValue)
+    }
     
-    $scope.inputValue = "";
+    $scope.addOn = function(num) {
+        console.log('inputvalue:', $scope.inputValue)
+        console.log('passed num', num);
+        if (num === '0') {
+            $scope.inputValue = '0'
+        }
+        $scope.inputValue = $scope.inputValue + num;
+    }
+    
+    console.log($scope.inputValue)
+    console.log(typeof($scope.inputValue), $scope.inputValue)
 
 })
